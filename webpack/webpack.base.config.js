e108555575260d65c659ac14f52b4f2db75b678a
last order <2495713984@qq.com>
2020-08-29 19:26:44 +0800

    修改config

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '../dist'),
-    filename: '[name].[hash:8].js',
+    filename: 'index.js',
     library: 'LoPlayer',
     libraryTarget: 'umd',
     libraryExport: 'default'
